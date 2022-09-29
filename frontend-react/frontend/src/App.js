8d8d6a4850fd0bb1285999f3f29e992382396a7f
Roxanne Reindl <rreindl13@gmail.com>
2022-09-28 17:35:02 -0700

    basic frontend react table POC with hard coded data

--- frontend-react/frontend/src/App.js
+++ frontend-react/frontend/src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Table from './components/table/Table'
+
+const list = [
+  { id: 1, name: "Joe Dean", Age: 22},
+  { id: 2, name: "Rox Dean", Age: 44},
+  { id: 3, name: "Rox Dean", Age: 44},
+  { id: 4, name: "Rox Dean", Age: 44},
+  { id: 5, name: "Rox Dean", Age: 44},
+  { id: 6, name: "Rox Dean", Age: 44},
+  { id: 7, name: "Rox Dean", Age: 44},
+  { id: 8, name: "Rox Dean", Age: 44},
+  { id: 9, name: "Rox Dean", Age: 44},
+  { id: 10, name: "Rox Dean", Age: 44},
+  { id: 11, name: "Rox Dean", Age: 44},
+]
+
+const colNames = ['Id', 'Name', 'Age']
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Table list={list} colNames={colNames} pageSize={3} />
     </div>
   );
 }
