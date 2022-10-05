21d16e91dbe3fa7571d4d8b8f7e165b114919912
Roxanne Reindl <rreindl13@gmail.com>
2022-10-04 17:00:57 -0700

    sorting for table

--- frontend-react/frontend/src/App.js
+++ frontend-react/frontend/src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 import Table from './components/table/Table'
 
 const list = [
-  { id: 1, name: "Joe Dean", Age: 22},
-  { id: 2, name: "Rox Dean", Age: 44},
-  { id: 3, name: "Rox Dean", Age: 44},
-  { id: 4, name: "Rox Dean", Age: 44},
+  { id: 44, name: "Joe Dean", Age: 66},
+  { id: 2, name: "Anita Dean", Age: 44},
+  { id: 3, name: "Rox Dean", Age: 33},
+  { id: 411, name: "Rox Dean", Age: 55},
   { id: 5, name: "Rox Dean", Age: 44},
-  { id: 6, name: "Rox Dean", Age: 44},
-  { id: 7, name: "Rox Dean", Age: 44},
-  { id: 8, name: "Rox Dean", Age: 44},
-  { id: 9, name: "Rox Dean", Age: 44},
+  { id: 6, name: "Bob Brown", Age: 22},
+  { id: 23, name: "Rox Dean", Age: 40},
+  { id: 8, name: "Rox Dean", Age: 29},
+  { id: 119, name: "Anna Wintor", Age: 44},
   { id: 10, name: "Rox Dean", Age: 44},
   { id: 11, name: "Rox Dean", Age: 44},
 ]
