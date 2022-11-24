e41d69d857c0faec87c0c897b3715cafcaedd74a
Roxanne Reindl <rreindl13@gmail.com>
2022-11-23 20:48:57 -0700

    Added edit user routing path to App.js

--- frontend-react/frontend/src/App.js
+++ frontend-react/frontend/src/App.js
@@ -10,6 +10,9 @@ import Student from "./pages/Student";
 import Home from "./pages/Home";
 import Faculty from "./pages/Faculty";
 import AddUser from './users/addUsers';
+import EditUser from './users/editUser';
+
+
 
 const list = [
   { id: 44, name: "Joe Dean", Age: 66},
@@ -38,6 +41,8 @@ function App() {
           <Route path= "/Student" element={<Student />} />
           <Route path= "/faculty" element={<Faculty />} />
           <Route path= "/addUser" element={<AddUser />} />
+          <Route path= "/editUser/:id" element={<EditUser />} />
+
         </Routes>
       </div>
     </>
